c21cf9de17e5d8c654a51e0b4dc613dcc6cad7f7
Nyirate <rwakaemma34@gmail.com>
2019-08-30 21:07:03 +0200

    add new codes

--- src/app/search-foorm/search-foorm.component.ts
+++ src/app/search-foorm/search-foorm.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-search-foorm',
   templateUrl: './search-foorm.component.html',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFoormComponent implements OnInit {
 
-  constructor() { }
+  Username: string = "";
+  response: any;
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
   }
-
+   search() {
+     this.http.get("https://api.github.com/users/daneden?access_token=c640142ec133d7db16859a0380e82ae43819a6d0" + this.Username)
+     .subscribe((response) => {
+       this.response = response;
+       console.log(this.response);
+     })
+    }
 }
