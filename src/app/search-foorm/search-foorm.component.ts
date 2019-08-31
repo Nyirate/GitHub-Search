9513a5c77378bcb43fd4d4c07ace71723ab5d337
Nyirate <rwakaemma34@gmail.com>
2019-08-31 19:30:28 +0200

    add bootstrap

--- src/app/search-foorm/search-foorm.component.ts
+++ src/app/search-foorm/search-foorm.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { resolveNs } from 'dns';
+import { resolve } from 'url';
 @Component({
   selector: 'app-search-foorm',
   templateUrl: './search-foorm.component.html',
@@ -14,21 +14,21 @@ export class SearchFoormComponent implements OnInit {
 
   ngOnInit() {
   }
-   search() {
-     let promise = new Promise ((resolve, reject)=>{
-      this.http.get("https://api.github.com/users/"+this.Username+"?access_token=c640142ec133d7db16859a0380e82ae43819a6d0")
-      .subscribe((emma) => {
-        this.emma = emma;
-        console.log(this.emma); 
-        resolve()
-     },
-     err=>{
-       this.emma.login = "No login"
-       this.emma.email = "incorrect"
+//    search() {
+//      let promise = new Promise ((resolve, reject)=>{
+//       this.http.get("https://api.github.com/users/"+this.Username+"?access_token=c640142ec133d7db16859a0380e82ae43819a6d0")
+//       .subscribe((emma) => {
+//         this.emma = emma;
+//         console.log(this.emma); 
+//         resolve()
+//      },
+//      err=>{
+//        this.emma.login = "No login"
+//        this.emma.email = "incorrect"
 
-       reject()
-     })
+//        reject()
+//      })
      
-     })
-    }
-}
+//      })
+//     }
+// }
