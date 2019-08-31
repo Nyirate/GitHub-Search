d7b6baf6930652b2037bcc0e17e24311d35d5e02
Nyirate <rwakaemma34@gmail.com>
2019-08-31 22:23:15 +0200

    add a repo class

--- src/app/search-foorm/search-foorm.component.ts
+++ src/app/search-foorm/search-foorm.component.ts
@@ -17,23 +17,23 @@ export class SearchFoormComponent implements OnInit {
 
   ngOnInit() {
   }
-   search() {
-     let promise = new Promise ((resolve, reject)=>{
-      this.http.get("https://api.github.com/users/"+this.Username+"?access_token=c640142ec133d7db16859a0380e82ae43819a6d0")
-      .subscribe((emma) => {
-        this.emma = emma;
-        console.log(this.emma); 
-        resolve()
-     },
-     _err=>{
-       this.emma.login = "No login"
-       this.emma.email = "incorrect"
+  //  search() {
+  //    let promise = new Promise ((resolve, reject)=>{
+  //     this.http.get("https://api.github.com/users/"+this.Username+"?access_token=c640142ec133d7db16859a0380e82ae43819a6d0")
+  //     .subscribe((emma) => {
+  //       this.emma = emma;
+  //       console.log(this.emma); 
+  //       resolve()
+  //    },
+  //    _err=>{
+  //      this.emma.login = "No login"
+  //      this.emma.email = "incorrect"
 
-       reject()
-     })
+  //      reject()
+  //    })
      
-     })
-    }
+  //    })
+  //   }
 
     // repoSearch(_Username0){
     //   interface ApiResponse{
